86273451345aafeec55bb67bfbda26170cfa011d
Matthew Shelley <matthew.shelley@thredup.com>
2016-06-03 15:46:07 -0700

    fixing error in style import bumping package

--- generators/app/templates/styles.js
+++ generators/app/templates/styles.js
@@ -1,7 +1,7 @@
 // Using inline CSS until we can get full CSS modules rolled out
 export default {
 
-  .<%= slug %> {
-    border: 1px solid #ccc;
+  '<%= slug %>': {
+    border: '1px solid #ccc'
   }
-}
+};
